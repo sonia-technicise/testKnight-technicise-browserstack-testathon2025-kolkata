8a6e96134f5e74b286761cd4a348f4dcb9bd7745
atanu-technicise <atanupandit.technicise@gmail.com>
2025-07-13 18:29:11 +0530

    Setup update for running in local chrome driver

--- java_test_code/junit-browserstack-master/junit-5/src/test/java/runners/BstackRunner.java
+++ java_test_code/junit-browserstack-master/junit-5/src/test/java/runners/BstackRunner.java
@@ -3,6 +3,9 @@ package runners;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,7 +20,7 @@ import java.util.*;
 
 public class BstackRunner {
     public WebDriver driver;
-    protected WebDriverWait wait;  
+    protected WebDriverWait wait;
 
     public static String userName, accessKey;
     public static Map<String, Object> browserStackYamlMap;
@@ -30,22 +33,47 @@ public class BstackRunner {
 
     @BeforeEach
     public void setUp() throws Exception {
-        MutableCapabilities capabilities = new MutableCapabilities();
-        userName = System.getenv("BROWSERSTACK_USERNAME") != null
-                ? System.getenv("BROWSERSTACK_USERNAME")
-                : (String) browserStackYamlMap.get("userName");
-        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY") != null
-                ? System.getenv("BROWSERSTACK_ACCESS_KEY")
-                : (String) browserStackYamlMap.get("accessKey");
+        String runEnv = System.getenv("RUN_ENV"); // "local" or null
 
-        HashMap<String, Object> bStackOptions = new HashMap<>();
-        bStackOptions.put("source", "junit5:sample-master:v1.2");
-        capabilities.setCapability("bstack:options", bStackOptions);
+        if ("local".equalsIgnoreCase(runEnv)) {
+            System.out.println("[Runner] Running tests locally with Chrome GUI");
+        
+            // Set up ChromeDriver with GUI
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(false); // Important: GUI mode
+            options.addArguments("--remote-allow-origins=*"); // Fix CDP issue
+            options.addArguments("--disable-dev-shm-usage"); // Avoid shared memory issue
+            options.addArguments("--no-sandbox"); // Sometimes required in Linux
+            options.addArguments("--start-maximized");
+            options.addArguments("--user-data-dir=/tmp/chrome-profile"); // Avoid CDP 403
+        
+            driver = new ChromeDriver(options);
+        }else {
+            //  BrowserStack setup
+            System.out.println("[Runner] Running tests on BrowserStack.");
+            MutableCapabilities capabilities = new MutableCapabilities();
 
-        driver = new RemoteWebDriver(
-                new URL(String.format("https://%s:%s@hub.browserstack.com/wd/hub", userName, accessKey)),
-                capabilities
-        );
+            userName = System.getenv("BROWSERSTACK_USERNAME") != null
+                    ? System.getenv("BROWSERSTACK_USERNAME")
+                    : (String) browserStackYamlMap.get("userName");
+
+            accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY") != null
+                    ? System.getenv("BROWSERSTACK_ACCESS_KEY")
+                    : (String) browserStackYamlMap.get("accessKey");
+
+            if (userName == null || accessKey == null) {
+                throw new IllegalStateException("Missing BrowserStack credentials. Set environment variables or check browserstack.yml.");
+            }
+
+            HashMap<String, Object> bStackOptions = new HashMap<>();
+            bStackOptions.put("source", "junit5:sample-master:v1.2");
+            capabilities.setCapability("bstack:options", bStackOptions);
+
+            driver = new RemoteWebDriver(
+                    new URL(String.format("https://%s:%s@hub.browserstack.com/wd/hub", userName, accessKey)),
+                    capabilities
+            );
+        }
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
